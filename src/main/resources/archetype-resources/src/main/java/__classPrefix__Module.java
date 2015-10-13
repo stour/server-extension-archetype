d74571559d015104e93b285fdfb09269b46463b3
Stéphane Tournié <stephane.tournie@serli.com>
2015-10-13 16:43:20 +0200

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/__classPrefix__Module.java
@@ -0,0 +1,29 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package ${package};
+
+import org.eclipse.che.inject.DynaModule;
+
+import com.google.inject.AbstractModule;
+
+@DynaModule
+public class ${classPrefix}Module extends AbstractModule {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void configure() {
+        bind(${classPrefix}Service.class);
+    }
+}
